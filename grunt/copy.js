fdfe80a8fda0dc049b0a97726f89280e62ba1997
DELTA\324722 <chellakumar.manonmani@delta.com>
2017-04-18 11:47:54 -0400

    adding clientlibifyConfig files

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,7 +1,9 @@
  module.exports = {
 	default: {
 		files: [
-			{expand: true, cwd: 'src/css/', src: ['**'], dest: 'compiled/css/'}
+			{expand: true, cwd: 'src/css/', src: ['**'], dest: 'compiled/css/'},
+			{expand: true, cwd: 'src/clientlibifyConfig/', src: ['clientlibify.js'], dest: 'node_modules/grunt-clientlibify/tasks/'},
+			{expand: true, cwd: 'src/clientlibifyConfig/', src: ['filter.xml'], dest: 'node_modules/grunt-clientlibify/tasks/templates/'}
 		]
 	},
 	gruntdev: {
